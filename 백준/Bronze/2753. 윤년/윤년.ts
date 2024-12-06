9d14fa82146a0545132f609b15fd82edce468b3e
ﾊﾑ <hemdezvous@gmail.com>
2024-12-06 22:17:26 +0900

    [Bronze V] Title: 윤년, Time: 100 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.ts
@@ -0,0 +1,3 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim();
+const year = parseInt(input);
+console.log((year%4 === 0 && year%100 !== 0) || year%400 === 0 ? "1" : "0");
