7cbc479243298fc7775795bf95b3d26d79f562bc
ﾊﾑ <hemdezvous@gmail.com>
2024-12-26 15:54:48 +0900

    [level 0] Title: 배열 조각하기, Time: 2.17 ms, Memory: 36.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.js
@@ -0,0 +1,10 @@
+function solution(arr, query) {
+    query.forEach((q, i) => {
+        if(i % 2 === 0) {
+            arr = arr.slice(0, q + 1);
+        } else {
+            arr = arr.slice(q);
+        }
+    })
+    return arr;
+}
