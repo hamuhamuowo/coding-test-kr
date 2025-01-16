c10a7cbd28afd389cca4b0314ab322e3dba7eedb
ﾊﾑ <hemdezvous@gmail.com>
2025-01-16 16:06:28 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 0.16 ms, Memory: 38.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.js
@@ -0,0 +1,9 @@
+function solution(n) {
+    const answer = [];
+    for(let i = 0; i < n; i++) {
+        const arr = new Array(n).fill(0);
+        arr[i] = 1;
+        answer.push(arr);
+    }
+    return answer;
+}
