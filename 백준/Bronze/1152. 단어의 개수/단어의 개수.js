18e6c08a1c94e95f1522fda9726b49e25c915306
ﾊﾑ <hemdezvous@gmail.com>
2025-03-19 17:51:54 +0900

    [Bronze II] Title: 단어의 개수, Time: 176 ms, Memory: 19180 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.js
@@ -0,0 +1,8 @@
+const input = require("fs").readFileSync("/dev/stdin").toString().trim().split(' ');
+let count = 0;
+for(let i = 0; i < input.length; i++) {
+    if(input[i] !== '') {
+        count++;
+    }
+}
+console.log(count);
