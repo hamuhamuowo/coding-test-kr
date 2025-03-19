22114486ad50675901d33ca3e6f160a2f28a7762
ﾊﾑ <hemdezvous@gmail.com>
2025-03-19 11:54:52 +0900

    [Bronze V] Title: A+B - 7, Time: 96 ms, Memory: 9536 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.js
@@ -0,0 +1,6 @@
+const [n, ...arr] = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
+const answer = [];
+for(let i = 0; i < parseInt(n); i++) {
+    const [a, b] = arr[i].split(' ').map(Number);
+    console.log(`Case #${i+1}: ${a+b}`);
+}
