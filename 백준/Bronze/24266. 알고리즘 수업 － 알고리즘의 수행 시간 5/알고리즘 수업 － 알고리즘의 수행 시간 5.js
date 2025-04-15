8e973506ef1b91d4d3bc91e017330fdb0f2738ab
ﾊﾑ <hemdezvous@gmail.com>
2025-04-15 15:54:51 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 92 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.js
@@ -0,0 +1,4 @@
+const input = require('fs').readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt").toString().trim();
+let num = BigInt(input);
+console.log(`${num * num * num}`);
+console.log(3);
